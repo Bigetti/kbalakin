378ab93df705487482c291c44795417d98ae71f8
KonstantinBalakin <upgrade81@mail.ru>
2018-01-09 18:17:02 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,20 @@
 package ru.job4j;
 
+/**
+*Class Класс для создания калькулятора
+*author kbalakin
+*@since 09.01.2018
+*version'
+*/
+
 public class Calculate {
 
-    public static void main(String[] args) {
+    /**
+	* Метод вывода строки в консоль
+	* @param arg -arg.
+	*/
+	
+	public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 } 
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+/**
+ *Package for calculate task.
+ *@author Konstantin Balakin(upgrade81@mail.ru)
+ *version 1.0
+ *@since 09.01.2018
+ */
+ 
+ package ru.job4j;
+ 
