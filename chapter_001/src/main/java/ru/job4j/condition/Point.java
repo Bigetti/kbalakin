7ba9e3ad04663d80da28e3eb5a94c1a144fb2115
KonstantinBalakin <upgrade81@mail.ru>
2018-02-27 10:55:59 +0300

    3.4. Расстояние между точками в системе координат[#42918]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -4,36 +4,31 @@ public class Point {
 	private int x;
 	private int y;
 	
-	public Point(int x, int y) {
-		this.x=x;
-		this.y=y;
+	public Point(int x, int y){
+		this.x = x;
+		this.y = y;
 	}
 	
 	public double distanceTo(Point that) {
-		Point a = this;
-		Point b = that;
-		int x1 = a.x;
-		int y1 = a.y;
-		int x2 = b.x;
-		int y2 = b.y;
-		double result = Math.sqrt(
-            Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-		return result;
+		// Точка А - это текущая точка. К ней мы обращаемся через оператор this.
+    Point a = this;
+    // Точка В - это входящая точка. К ней мы можем обратиться напрямую через имя переменной that.
+    // или для удоства мы создали новую переменню b и к ней присвоили переменную this.
+    Point b = that;
+    // сделаем вывод на консоль.
+    System.out.println("x1 = " + a.x);
+    System.out.println("y1 = " + a.y);
+    System.out.println("x2 = " + b.x);
+    System.out.println("y2 = " + b.y);
+    double result = Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
+	return result;
 	}
 	
 	public static void main(String[] args) {
         Point a = new Point(0, 1);
         Point b = new Point(2, 5);
-		a.distanceTo(b);
-		// сделаем вызов метода
-		System.out.println("x1 = " + a.x);
-		System.out.println("y1 = " + a.y);
-		System.out.println("x2 = " + b.x);
-		System.out.println("y2 = " + b.y);
-		
 		double result = a.distanceTo(b);
-		System.out.println("Расстояние между точками А и В : " + result);
-    }
-	
+	    System.out.println("Расстояние между точками А и В : " + result);
+	}
 	
 }
