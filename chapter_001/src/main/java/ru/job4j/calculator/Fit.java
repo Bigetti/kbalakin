50d1be656759730d8b6085c1d502eb3f0678ae3b
KonstantinBalakin <upgrade81@mail.ru>
2018-02-27 12:07:09 +0300

    Числа в константы  3.3. Идеальный вес.[#42821]

--- chapter_001/src/main/java/ru/job4j/calculator/Fit.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Fit.java
@@ -4,14 +4,17 @@ package ru.job4j.calculator;
 * Программа рассчета идеального веса.
 */
 public class Fit {
-	
+
+	public static final int MAN_MINUS = 100;
+	public static final int WOM_MINUS = 110;
+	public static final double KOFF = 1.15;
 	/**
 	* Идеальный вес для мужчины.
 	* @param height Рост.
 	*@ return идеальный вес.
 	*/
 	static double manWeight(double height) {
-		return (height - 100)*1.15;
+		return (height - MAN_MINUS)*KOFF;
 	}
 	
 	/**
@@ -20,7 +23,7 @@ public class Fit {
 	*@return идеальный вес.
 	*/
 	static double womanWeight(double height) {
-		return (height - 110)*1.15;
+		return (height - WOM_MINUS)*KOFF;
 	}
 	
 	public static void main(String[] args) {
