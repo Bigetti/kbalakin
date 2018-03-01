383743ceb2f1a83b95ca1d9caad474a54628aa10
KonstantinBalakin <upgrade81@mail.ru>
2018-03-01 10:08:10 +0300

    3.2. Максимум из двух чисел[#42961] Оформление поправил.пробелы в предложении убрал.

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,13 +1,11 @@
 package ru.job4j.max;
 
+
 public class Max {
-
-
 
 	public int max(int first, int second) {
 
-
-		return (first>second)?first:second;
+		return (first > second) ? first : second;
 	}
 	
 	public int summation(int first, int second) {
