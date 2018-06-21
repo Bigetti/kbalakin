d6689d3f10ce4c3d59c7d8d9878ec3f49ffd81e2
KonstantinBalakin <upgrade81@mail.ru>
2018-06-21 10:41:14 +0300

    5.7. Квадратный массив заполнен true или false по диагоналям. [#53859[#54562]  можно упростить. != break и тогда убрать else.

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -2,16 +2,13 @@ package ru.job4j.array;
 
 public class MatrixCheck {
     public boolean mono(boolean[][] data) {
-        boolean result = false;
-        for (int i = 1; i <= data.length - 1; i++) {
-            if (data[0][0] = data[i][i]) {
-                result = true;
-            }
-            else {
+        boolean result = true;
+        for (int i = 1; i < data.length; i++) {
+            if (data[0][0] != data[i][i] || data[2][0] != data[2-i][i]) {
                 result = false;
-                break;
             }
         }
         return result;
+
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -9,8 +9,8 @@ public class MatrixCheckTest {
     @Test
     public void whenDataMonoByTrueThenTrue() {
         MatrixCheck check = new MatrixCheck();
-        boolean[][] input = new boolean[][] {
-                {true, true, false},
+        boolean[][] input = new boolean[][]{
+                {true, true, true},
                 {false, true, true},
                 {true, false, true}
         };
@@ -21,7 +21,7 @@ public class MatrixCheckTest {
     @Test
     public void whenDataNotMonoByTrueThenFalse() {
         MatrixCheck check = new MatrixCheck();
-        boolean[][] input = new boolean[][] {
+        boolean[][] input = new boolean[][]{
                 {true, true, false},
                 {false, false, true},
                 {true, false, true}
@@ -29,4 +29,29 @@ public class MatrixCheckTest {
         boolean result = check.mono(input);
         assertThat(result, is(false));
     }
+
+    @Test
+    public void whenDataMonoByFalseThenFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][]{
+                {false, true, false},
+                {false, false, true},
+                {false, false, false}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenOneisTrueAndOneIsFalse() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][]{
+                {true, true, false},
+                {false, false, true},
+                {false, false, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
 }
+
