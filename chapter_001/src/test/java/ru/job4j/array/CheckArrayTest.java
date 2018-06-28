e9bf2366f45a6d19db9da6146ad6d24f7ee4adde
KonstantinBalakin <upgrade81@mail.ru>
2018-06-28 12:00:27 +0300

    ДОП ЗАД 1: проверить массив на предмет отсортированности по возрастанию

--- chapter_001/src/test/java/ru/job4j/array/CheckArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/array/CheckArrayTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertThat;
 
 public class CheckArrayTest {
     @Test
-    public void whenSortArrayIsUpThenTrue() {
+    public void whenSortArrayIsUpThenFalse() {
 
         CheckArray checksorter = new CheckArray();
         int[] input = new int[]{1, 7, 3, 4, 2, 2};
@@ -19,8 +19,19 @@ public class CheckArrayTest {
     public void whenSortArrayIsUpThenTrue() {
 
         CheckArray checksorter = new CheckArray();
-        int[] input = new int[]{1, 7, 3, 4, 2, 2};
+        int[] input = new int[]{1, 2, 3, 4, 7, 8};
+        boolean rst = checksorter.checksort(input);
+        assertThat(rst, is(true));
+    }
+
+
+    @Test
+    public void whenSortArrayWithDubllThenFalse() {
+
+        CheckArray checksorter = new CheckArray();
+        int[] input = new int[]{1, 2, 4, 4, 7, 8};
         boolean rst = checksorter.checksort(input);
         assertThat(rst, is(false));
     }
+
 }
